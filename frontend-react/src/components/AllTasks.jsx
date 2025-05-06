bfe945d84762d644d8086637fe3a43d1419dc9c5
Raquel Ferreira <rraquelof@gmail.com>
2025-05-06 20:03:47 -0300

    adicionando componente allTasks

--- frontend-react/src/components/AllTasks.jsx
+++ frontend-react/src/components/AllTasks.jsx
@@ -1,1 +1,14 @@
 //criar componente
+// src/components/AllTasks.jsx
+import React from 'react';
+
+const AllTasks = () => {
+  return (
+    <div>
+      <h2>Minhas Tarefas</h2>
+      <p>Componente básico</p>
+    </div>
+  );
+};
+
+export default AllTasks;
